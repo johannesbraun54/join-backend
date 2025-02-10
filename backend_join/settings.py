9a9b0eb7c06d5975f2382e08eb26795f8a41061c
johannesbraun54 <johannesbraun02@gmail.com>
2025-02-10 09:55:56 +0100

    ready api

--- backend_join/settings.py
+++ backend_join/settings.py
@@ -78,6 +78,11 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5500',
 
     'http://localhost:5500',
+    
+    'http://127.0.0.1:5501',
+
+    'http://localhost:5501',
+
 
 ]
 ROOT_URLCONF = 'backend_join.urls'
