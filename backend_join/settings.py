3ffb2624d0ba9337a10069c0f79c62315a044344
Johannes Braun <johannesbraun02@gmail.com>
2025-08-28 09:12:03 +0000

    refactor: added allowed hosts

--- backend_join/settings.py
+++ backend_join/settings.py
@@ -77,14 +77,17 @@ CSRF_TRUSTED_ORIGINS = [
 
 CORS_ALLOWED_ORIGINS = [
 
-    'http://127.0.0.1:5500',
+    	'http://127.0.0.1:5500',
 
-    'http://localhost:5500',
-    
-    'http://127.0.0.1:5501',
+    	'http://localhost:5500',
 
-    'http://localhost:5501',
+    	'http://127.0.0.1:5501',
 
+    	'http://localhost:5501',
+
+	'join-backend.jb-webdevelopment.com',
+
+	'34.1.166.178'
 
 ]
 ROOT_URLCONF = 'backend_join.urls'
